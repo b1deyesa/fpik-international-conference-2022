c034fb95e7d65e58c24c5de31e0fcee232b865f3
b1deyesa <bideyesa@gmail.com>
2022-08-27 10:53:37 +0700

    meta clear cache

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="cache-control" content="no-cache">
     
     <title>{{ config('app.name') }}</title>
     <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
